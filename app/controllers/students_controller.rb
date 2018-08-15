ca456fbb51ced05462e41f15fa443e39f94213d0
ritz-w <ritz.wu@outlook.com>
2018-08-15 21:48:52 +0100

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,7 +21,12 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    if params[:q]
+      @students = Student.search(params[:q])
+    end
+  end
+
+  def search
   end
 
   def student_params
